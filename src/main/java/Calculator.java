abdc7978049b213e092a16003ec63ae89f01e292
Pandu W <puthut.pandu13@gmail.com>
2026-09-20 09:50:31 +0700

    Initial Java project

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+
+    // add
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+// substract
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+//multiply
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
